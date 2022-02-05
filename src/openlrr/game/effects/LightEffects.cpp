7f15ba31b5e66fe3895862b19a467702a16b22da
Robert Jordan <jrob800@gmail.com>
2022-02-04 23:32:35 -0500

    Skeleton: LightEffects module
    * Implement all LightEffects types.
    * Declare all LightEffects function pointers.

---
+++ src/openlrr/game/effects/LightEffects.cpp
@@ -0,0 +1,24 @@
+// LightEffects.cpp : 
+//
+
+#include "LightEffects.h"
+
+
+/**********************************************************************************
+ ******** Globals
+ **********************************************************************************/
+
+#pragma region Globals
+
+// <LegoRR.exe @004ebdd8>
+LegoRR::LightEffects_Globs & LegoRR::lightGlobs = *(LegoRR::LightEffects_Globs*)0x004ebdd8;
+
+#pragma endregion
+
+/**********************************************************************************
+ ******** Functions
+ **********************************************************************************/
+
+#pragma region Functions
+
+#pragma endregion
