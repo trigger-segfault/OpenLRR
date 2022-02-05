70f325cb718a842abc1270fad787a0c47710a948
Robert Jordan <jrob800@gmail.com>
2022-02-04 23:13:18 -0500

    Skeleton: DamageText module
    * Implement all DamageText types.
    * Declare all DamageText function pointers.

---
+++ src/openlrr/game/effects/DamageText.cpp
@@ -0,0 +1,24 @@
+// DamageText.cpp : 
+//
+
+#include "DamageText.h"
+
+
+/**********************************************************************************
+ ******** Globals
+ **********************************************************************************/
+
+#pragma region Globals
+
+// <LegoRR.exe @004b9a58>
+LegoRR::DamageFont_Globs & LegoRR::dmgfontGlobs = *(LegoRR::DamageFont_Globs*)0x004b9a58;
+
+#pragma endregion
+
+/**********************************************************************************
+ ******** Functions
+ **********************************************************************************/
+
+#pragma region Functions
+
+#pragma endregion
